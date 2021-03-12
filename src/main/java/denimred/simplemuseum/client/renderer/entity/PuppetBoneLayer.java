84193f500d4424d1e5f5323a74148f88843dda55
Denim <76877830+DenimRed@users.noreply.github.com>
2021-03-12 10:29:09 -0700

    Add and apply license header
    Also:
    - Updated README.md to reflect actual versions.
    - Made LICENSE be included in the built jar.
    - Ensured .cache/ was excluded in the built jar.

--- src/main/java/denimred/simplemuseum/client/renderer/entity/PuppetBoneLayer.java
+++ src/main/java/denimred/simplemuseum/client/renderer/entity/PuppetBoneLayer.java
@@ -1,3 +1,21 @@
+/*
+ * Simple Museum
+ * Copyright (C) 2021 DenimRed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package denimred.simplemuseum.client.renderer.entity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
@@ -22,10 +40,10 @@ public enum PuppetBoneLayer {
     TRANSLUCENT(bone -> bone.name.endsWith("_slime"), RenderType::getEntityTranslucent);
 
     private final Predicate<GeoBone> predicate;
-    private final @Nullable
-    Function<ResourceLocation, RenderType> type;
+    private final @Nullable Function<ResourceLocation, RenderType> type;
 
-    PuppetBoneLayer(Predicate<GeoBone> predicate, @Nullable Function<ResourceLocation, RenderType> type) {
+    PuppetBoneLayer(
+            Predicate<GeoBone> predicate, @Nullable Function<ResourceLocation, RenderType> type) {
         this.predicate = predicate;
         this.type = type;
     }
