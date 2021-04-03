9bb92a4ddee892336007f448c559be018405a129
Denim <76877830+DenimRed@users.noreply.github.com>
2021-04-02 18:35:05 -0600

    Remove new line from header
    Also adds header to certain files that didn't have it.

--- src/main/java/denimred/simplemuseum/common/network/NetworkUtil.java
+++ src/main/java/denimred/simplemuseum/common/network/NetworkUtil.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package denimred.simplemuseum.common.network;
 
 import net.minecraft.client.world.ClientWorld;
--- src/main/java/denimred/simplemuseum/common/util/MathUtil.java
+++ src/main/java/denimred/simplemuseum/common/util/MathUtil.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package denimred.simplemuseum.common.util;
 
 import net.minecraft.util.math.vector.Vector3d;
